ca86dd5b6d264c63b851128823b2cbd3a2528d72
JDarkGreen <jgomez.4net@gmail.com>
2016-08-26 18:18:02 -0500

    actualizacion facebook fan page v2,7 -b 260816

--- themes/custom_theme_cingetec/partials/common/section-facebook.php
+++ themes/custom_theme_cingetec/partials/common/section-facebook.php
@@ -10,17 +10,17 @@ if( isset($facebook_link) && !empty($facebook_link) ) :
 	<!-- Contebn -->
 	<div id="fb-root" class=""></div>
 
-	<!-- Script -->
+	<!-- Script version 2.7 -->
 	<script>(function(d, s, id) {
-		var js, fjs = d.getElementsByTagName(s)[0];
-		if (d.getElementById(id)) return;
-		js = d.createElement(s); js.id = id;
-		js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.5";
-		fjs.parentNode.insertBefore(js, fjs);
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.7";
+	  fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));</script>
 
-	<div class="fb-page" data-href="<?= $facebook_link; ?>" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-width="445" data-height="360" data-hide-cover="false" data-show-facepile="true">
-	</div> <!-- /. fb-page-->
+	<div class="fb-page" data-href="<?= $facebook_link; ?>" data-tabs="timeline" data-small-header="false" data-width="445" data-height="360" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"></div><!-- /. fb-page-->
+
 </section> <!-- /.container__facebook -->
 
 <?php else: ?>
